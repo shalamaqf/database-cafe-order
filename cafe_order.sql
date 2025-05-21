2c9ec8339edf8793897d3b430900013f2cc12e62
Shalama Fadilla <shalamafadillaa@gmail.com>
2025-05-21 13:41:36 +0700

    create an enum type
    create an enum type for payment_methodin menu_order table.

--- cafe_order.sql
+++ cafe_order.sql
@@ -1,8 +1,13 @@
+-- create an enum type for payment_method
+CREATE TYPE method AS ENUM('cash', 'credit', 'e-money');
+
 -- create a table for customer
-create table customer
+CREATE TABLE customer
 (
-    customer_id SERIAL PRIMARY KEY,
+    customer_id serial PRIMARY KEY,
     customer_name varchar (100) not null,
     customer_email varchar (100) not null,
     loyalty_points int 
 );
+
+

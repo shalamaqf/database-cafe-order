998eee91ccc0d9091e4a1038eb26a5af510cd472
Shalama Fadilla <shalamafadillaa@gmail.com>
2025-05-21 13:46:26 +0700

    create a table for menu_order
    create new table for menu_order includes its attributes, such as order_id, customer_id, menu_name, menu_price, and payment_method. Where order_id is the primary key and customer_id as foreign key from customer table.

--- cafe_order.sql
+++ cafe_order.sql
@@ -10,4 +10,14 @@ CREATE TABLE customer
     loyalty_points int 
 );
 
+-- create a table for menu order
+create table menu_order
+(
+    order_id SERIAL PRIMARY KEY,
+    customer_id int not null,
+    menu_name varchar (100) not null,
+    menu_price int not null,
+    payment_method method not null,
+    CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
+);
 
